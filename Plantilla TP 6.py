2ad6339dadc810e2a86b8a2181bd533554881608
Eimi Saiz <83459802+EimiSaiz@users.noreply.github.com>
2021-06-22 00:24:16 -0300

    Plantilla del grupo.

---
+++ Plantilla TP 6.py
@@ -0,0 +1,13 @@
+########################################################
+# Matías G. Quevedo - @Gerchu-arqv
+# Eimi Saiz - @EimiSaiz
+# TP 6 - Eje. 1: Xxx
+# UNRN Andina - Introducción a Ingeniería en Computación
+########################################################
+
+def prueba():
+
+    pass
+
+if __name__ == "__main__":
+    prueba()
